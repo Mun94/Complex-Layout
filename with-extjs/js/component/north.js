e6002a071dc62abffabe6402901c0886af6e3b42
Mun94 <seongwoo720@gmail.com>
2021-07-27 14:57:11 +0900

    asdf

--- with-extjs/js/component/north.js
+++ with-extjs/js/component/north.js
@@ -1,7 +1,7 @@
-Ext.define('js.component.north', {
+Ext.define('mvc.component.north', {
     extend:'Ext.Component',
     region:'north',
-    html: js.data.description.includePTag('north'),
+    html: mvc.data.data.includePTag('north'),
     border: false,
     margin: '0 0 5 0',
 });
